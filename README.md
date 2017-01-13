3b447eb3667a2214e2dcd3939c9dc5dd3aeec454
堀祐磨 <hori_yuma@liginc.co.jp>
2017-01-13 13:04:16 +0900

    Upgrade code for yeoman-generator 1.x

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
-# generator-holiday-generator [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
-> 
+# generator-ejs-bem-component
+
+A generator of HTML view component
 
 ## Installation
 
-First, install [Yeoman](http://yeoman.io) and generator-holiday-generator using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+First, install [Yeoman](http://yeoman.io) and generator-ejs-bem-component using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
 
 ```bash
 npm install -g yo
-npm install -g generator-holiday-generator
+npm link
 ```
 
-Then generate your new project:
+Then generate your new component:
 
 ```bash
-yo holiday-generator
+yo ejs-bem-component [[component name]]
 ```
 
 ## Getting To Know Yeoman
@@ -25,11 +26,11 @@ Yeoman has a heart of gold. He&#39;s a person with feelings and opinions, but he
 MIT © [Yuma Hori]()
 
 
-[npm-image]: https://badge.fury.io/js/generator-holiday-generator.svg
-[npm-url]: https://npmjs.org/package/generator-holiday-generator
-[travis-image]: https://travis-ci.org//generator-holiday-generator.svg?branch=master
-[travis-url]: https://travis-ci.org//generator-holiday-generator
-[daviddm-image]: https://david-dm.org//generator-holiday-generator.svg?theme=shields.io
-[daviddm-url]: https://david-dm.org//generator-holiday-generator
-[coveralls-image]: https://coveralls.io/repos//generator-holiday-generator/badge.svg
-[coveralls-url]: https://coveralls.io/r//generator-holiday-generator
+[npm-image]: https://badge.fury.io/js/generator-ejs-bem-component.svg
+[npm-url]: https://npmjs.org/package/generator-ejs-bem-component
+[travis-image]: https://travis-ci.org//generator-ejs-bem-component.svg?branch=master
+[travis-url]: https://travis-ci.org//generator-ejs-bem-component
+[daviddm-image]: https://david-dm.org//generator-ejs-bem-component.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org//generator-ejs-bem-component
+[coveralls-image]: https://coveralls.io/repos//generator-ejs-bem-component/badge.svg
+[coveralls-url]: https://coveralls.io/r//generator-ejs-bem-component
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,51 +1,58 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+const yeoman = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
 
-module.exports = yeoman.generators.Base.extend({
-  constructor: function () {
-    yeoman.generators.Base.apply(this, arguments);
+module.exports = yeoman.extend({
+
+  constructor: function (args, opts) {
+    yeoman.apply(this, arguments);
     this.argument('moduleName', { type: String, required: false, optional: true });
   },
-  prompting: function () {
-    if (typeof this.moduleName !== 'undefined') { return true; }
-    var done = this.async();
 
-    var prompts = [{
+  prompting: function () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the primo ' + chalk.red('generator-ejs-bem-component') + ' generator!'
+    ));
+
+    const prompts = [{
+      type: 'input',
       name: 'moduleName',
-      message: 'type new module name, please',
+      message: 'Type new component name, please',
       default: this.modulename
     }];
 
-    this.prompt(prompts, function (props) {
+    return this.prompt(prompts).then(function (props) {
+      // To access props later use this.props.someAnswer;
       this.props = props;
-      // To access props later use this.props.someOption;
       this.moduleName = this.props.moduleName;
-      done();
     }.bind(this));
   },
 
   writing: function () {
-    var baseName = this.moduleName;
-    var ejsName = '_' + baseName + '.ejs';
-    var scssName = '_' + baseName + '.scss';
+    const baseName = this.moduleName;
+    const ejsName = '_' + baseName + '.ejs';
+    const scssName = '_' + baseName + '.scss';
 
     this.fs.copyTpl(
       this.templatePath('_template.ejs'),
       this.destinationPath(baseName + '/' + ejsName),
       { name:baseName, end:'%>' }
     );
+
     this.fs.copyTpl(
       this.templatePath('_template.scss'),
       this.destinationPath(baseName + '/' + scssName),
       { name:baseName }
     );
+
     this.fs.copyTpl(
       this.templatePath('index.ejs'),
       this.destinationPath(baseName + '/' + 'index.ejs'),
       { name:baseName, end: '%>' }
     );
+
     this.fs.copyTpl(
       this.templatePath('_template-guide.ejs'),
       this.destinationPath(baseName + '/' + '_' + baseName + '-guide.ejs'),
